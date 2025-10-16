4edc2b6a405e4c421c8c80ff120470caf93e8534
Sandwichie123 <152046549+Sandwichie123@users.noreply.github.com>
2025-10-16 19:45:28 -0400

    started drive subsystem and updated constants

---
+++ src/main/java/com/team271/frc2023/subystems/Constants.java
@@ -0,0 +1,13 @@
+package com.team271.frc2023.subystems;
+
+public class Constants {
+    public int FRONT_DRIVER_ID = 1;
+    public int BACK_DRIVER_ID = 3;
+    public int FRONT_PASSENGER_ID = 2;
+    public int BACK_PASSENGER_ID = 5;
+
+    public int JOYSTICK_PORT_NUMBER = 0;
+    public int KALI_SPEED_AXIS_NUMBER = 1;
+    public int ROTATION_AXIS_NUMBER = 0;
+    
+}
